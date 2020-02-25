54e2aa93bb4caa7dd604563d13003c22a5a6e963
zhizhuo song <forsongzz@hotmail.com>
2020-02-25 23:34:05 +0000

    把本地工程添加到git

---
+++ git_test_project/git_test/src/com/szz/git/FirstClass.java
@@ -0,0 +1,17 @@
+package com.szz.git;
+
+/**
+ * This Class
+ *
+ * @author Zhizhuo Song
+ * @version 2020/2/25
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
